27d9f458ab53eb97b0d6f78f255deaf44153c30c
Oliver Skroblin <o.skroblin@shopware.com>
2019-04-29 15:53:35 +0200

    NEXT-2634 - Move suggest and product gateway to core. Move filter, pagination and sorting handler to event listener NEXT-2634 - Added events for suggest and search gateway NEXT-2634 - Refactor product and media demo data generator NEXT-2681 - Fix caching bug NEXT-2634 - Remove unused ProductServiceFetchDefinition NEXT-2634 - Refactor ProductPriceDefinitionBuilder to have only one public function

---
+++ Content/Product/SalesChannel/Suggest/ProductSuggestGatewayInterface.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Content\Product\SalesChannel\Suggest;
+
+use Shopware\Core\Framework\DataAbstractionLayer\Search\EntitySearchResult;
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
+use Symfony\Component\HttpFoundation\Request;
+
+interface ProductSuggestGatewayInterface
+{
+    public function suggest(Request $request, SalesChannelContext $context): EntitySearchResult;
+}
