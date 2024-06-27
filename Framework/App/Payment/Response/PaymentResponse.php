96ef9ac13a274d1371a27d122d0eb7e29de36b67
Max Stegmeyer <m.stegmeyer@shopware.com>
2024-06-27 12:43:47 +0000

    NEXT-31047 - Improve payment handlers

--- Framework/App/Payment/Response/PaymentResponse.php
+++ Framework/App/Payment/Response/PaymentResponse.php
@@ -6,23 +6,31 @@ use Shopware\Core\Framework\Log\Package;
 use Shopware\Core\System\StateMachine\Aggregation\StateMachineTransition\StateMachineTransitionActions;
 
 /**
- * @internal only for use by the app-system
+ * @internal
  */
 #[Package('checkout')]
-class RefundResponse extends AbstractResponse
+class PaymentResponse extends AbstractResponse
 {
     /**
-     * One of the possible transaction statuses based on status set in refund-response.
-     * Usually, this is one of: complete, fail, reopen
-     * By default, the refund will remain on 'open'
-     *
-     * @see StateMachineTransitionActions
+     * One of the possible transaction statuses based on status set in pay-response.
+     * Usually, this is one of: paid, paid_partially, authorize, process, fail
+     * By default, the payment will remain on 'open'
      */
     protected ?string $status = null;
+
+    /**
+     * This is the URL the user is redirected to after the app has received the order data.
+     */
+    protected ?string $redirectUrl = null;
 
     public function getStatus(): ?string
     {
         return $this->status;
+    }
+
+    public function getRedirectUrl(): ?string
+    {
+        return $this->redirectUrl;
     }
 
     public function getErrorMessage(): ?string
@@ -32,7 +40,7 @@ class RefundResponse extends AbstractResponse
         }
 
         if ($this->status === StateMachineTransitionActions::ACTION_FAIL) {
-            return 'Refund was reported as failed.';
+            return 'Payment was reported as failed.';
         }
 
         return null;
