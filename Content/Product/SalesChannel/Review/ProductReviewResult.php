33aaa807810f25973ada4d4bc100146e1e2af33c
akf-bw <benjamin.wittwer@a-k-f.de>
2024-09-05 15:06:06 +0200

    NEXT-36468 - Move ProductReviewLoader logic to core
    fixes https://github.com/shopware/shopware/pull/3671

--- Content/Product/SalesChannel/Review/ProductReviewResult.php
+++ Content/Product/SalesChannel/Review/ProductReviewResult.php
@@ -48,6 +48,11 @@ class ProductReviewResult extends EntitySearchResult
      */
     protected $totalReviews;
 
+    /**
+     * @var int
+     */
+    protected $totalNativeReviews;
+
     public function getProductId(): string
     {
         return $this->productId;
@@ -86,6 +91,16 @@ class ProductReviewResult extends EntitySearchResult
     public function setTotalReviews(int $totalReviews): void
     {
         $this->totalReviews = $totalReviews;
+    }
+
+    public function getTotalNativeReviews(): int
+    {
+        return $this->totalNativeReviews;
+    }
+
+    public function setTotalNativeReviews(int $totalNativeReviews): void
+    {
+        $this->totalNativeReviews = $totalNativeReviews;
     }
 
     public function getParentId(): ?string
