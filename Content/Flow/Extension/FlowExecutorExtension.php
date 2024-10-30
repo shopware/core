a75cded2ca5b50afcd62f638ab3a6fd6dc494594
Oliver Skroblin <oliver@goblin-coders.de>
2024-10-30 11:23:03 +0100

    NEXT-39366 - Added extension point for flow executor fixes #5336

---
+++ Content/Flow/Extension/FlowExecutorExtension.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Content\Flow\Extension;
+
+use Shopware\Core\Content\Flow\Dispatching\StorableFlow;
+use Shopware\Core\Content\Flow\Dispatching\Struct\Flow;
+use Shopware\Core\Framework\Extensions\Extension;
+use Shopware\Core\Framework\Log\Package;
+
+/**
+ * This extension allows you to control the flow of execution or to pre-load and post-load specific data, enabling added monitoring capabilities or the ability to trigger external services.
+ *
+ * @extends Extension<void>
+ */
+#[Package('services-settings')]
+final class FlowExecutorExtension extends Extension
+{
+    public const NAME = 'flow.executor';
+
+    /**
+     * @internal shopware owns the __constructor, but the properties are public API
+     */
+    public function __construct(
+        public readonly Flow $flow,
+        public readonly StorableFlow $event
+    ) {
+    }
+}
