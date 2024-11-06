de27316a70158b94e77f34b207e666bf1a081e08
Dennis Garding <d.garding@shopware.com>
2024-11-06 07:13:35 +0100

    NEXT-39366 - Add annotations

--- Content/Flow/Extension/FlowExecutorExtension.php
+++ Content/Flow/Extension/FlowExecutorExtension.php
@@ -8,9 +8,17 @@ use Shopware\Core\Framework\Extensions\Extension;
 use Shopware\Core\Framework\Log\Package;
 
 /**
- * This extension allows you to control the flow of execution or to pre-load and post-load specific data, enabling added monitoring capabilities or the ability to trigger external services.
+ * @public
+ *
+ * @title Flow Executor Extension
+ *
+ * @description This extension allows you to control the flow of execution or to pre-load and post-load specific data, enabling added monitoring capabilities or the ability to trigger external services.
  *
  * @extends Extension<void>
+ *
+ * @experimental stableVersion:v6.7.0 feature:EXTENSION_SYSTEM
+ *
+ * @codeCoverageIgnore
  */
 #[Package('services-settings')]
 final class FlowExecutorExtension extends Extension
